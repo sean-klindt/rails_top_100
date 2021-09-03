2707c9c761a4e400d0591e1378e58a5ec8f58808
“Meg <meg5ed@virginia.edu>
2021-09-02 20:47:49 -0600

    added billboard

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,50 +1,52 @@
 class BillboardsController < ApplicationController
   def index
     @billboards = Billboard.all
-    render component: 'Billboards', props:{ billboards: @billboards }
-    end
-  
-    
-    def show
+    render component: 'Billboards', props: { billboards: @billboards }
+  end
+
+  def show
     @billboard = Billboard.find(params[:id])
-    render component: 'Billboard', props: { Billboard: @Billboard }
-    end
-  
-    def new
+    render component: 'Billboard', props: { billboard: @billboard }
+  end
+
+  def new
     @billboard = Billboard.new
-    render component: 'BillboardNew', props: { billboard: @billboard }
+    render component: 'Billboard', props: { billboard: @billboard }
+  end
+
+  def create 
+    @billboard = Billboard.new(billboard_params)
+    if @billboard.save 
+      redirect_to billboards_path
+    else
+      render component: 'BillboardNew', props: { billboard: @billboard }
     end
-  
-    def edit
+  end
+
+  def edit
     @billboard = Billboard.find(params[:id])
     render component: 'BillboardEdit', props: { billboard: @billboard }
-    end
-  
-    def create
-      @billboard = Billboard.new(billboard_params)
-      if @billboard.save
-        do something or do somewhere
-      else
-        render component: 'BillboardNew', props: { billboard: @billboard }
-      end
-    end
-  
-    def update
-      @billboard = Billboard.find(params[:id])
-      if @billboard.update(billboard_params)
-        redirect_to billboards_path
-      else
-        render component: 'BillboardEdit', props: { billboard: @billboard }
-      end
-    end
-  
-    def destroy
-      Billboard.find(params[:id]).destroy
+  end
+
+  def update 
+    @billboard = Billboard.find(params[:id])
+    if @billboard.update(billboard_params)
       redirect_to billboards_path
+    else
+      render component: 'BillboardEdit', props: { billboard: @billboard }
     end
+  end
+
+  def destroy
+    @billboard = Billboard.find(params[:id])
+    @billboard.destroy
+    redirect_to billboards_path
   
-    private
-    def billboard_params
-      params.require(:billboard).permit(:artist, :song_title, :album, :week_num)
-    end
+  end
+
+
+  private
+  def billboard_params
+    params.require(:billboard).permit(:artist, :song_title, :album, :week_num)
+  end
 end
